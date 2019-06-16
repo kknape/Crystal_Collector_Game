0dd8d6e6a041b44b889b265c8361d2c165dc4030
k2k <kadonna@wonderkul.com>
2019-06-16 16:19:19 -0500

    Making progress. Picking random values, assigning it to the crystal. Working on counter, scorre and reset.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,1 +1,44 @@
 //javascript file
+$(document).ready(function() { 
+
+
+var ramdomNumber = 0; 
+var counter = 0;
+var wins = 0; 
+var losses = 0; 
+
+
+// App picks a random number between 19 - 120
+function getRandomNumber() {
+    randomNumber = [Math.floor(Math.random() * 101) + 19];
+    $("#theNumber").html(randomNumber);
+   }
+
+getRandomNumber();
+
+
+function getCrystalValues() {
+    for (var i=0; i < 4; i++) {
+        var random = [Math.floor(Math.random() * 12) + 1];
+        
+        var crystal =$("<div>");
+            crystal.attr({
+                "class": "crystal", "data-randomNumber": random
+            });
+
+        $(".crystals").append(crystal);
+        }    
+    }
+
+getCrystalValues(); 
+
+//user clicks on a crystal
+$(".crystal").on("click", function(){
+    var crystalNum = parseInt($(this).attr("data-randomNumber"));
+    counter += crystalNum;
+
+    console.log(counter);
+});
+
+});
+
