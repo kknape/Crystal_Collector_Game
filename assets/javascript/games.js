5c3cd2f31c5f2c79e160eb7e16db2518a6f05f55
k2k <kadonna@wonderkul.com>
2019-06-16 21:59:03 -0500

    working up until reseting. When I reset i end up creating more divs. Tried a couple of things but doesn't seem to be working.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -9,15 +9,12 @@ var losses = 0;
 
 
 // App picks a random number between 19 - 120
-function getRandomNumber() {
+function startGame() {
+    $(".cyrstals").empty();
+
     randomNumber = [Math.floor(Math.random() * 101) + 19];
     $("#theNumber").html(randomNumber);
-   }
 
-getRandomNumber();
-
-
-function getCrystalValues() {
     for (var i=0; i < 4; i++) {
         var random = [Math.floor(Math.random() * 12) + 1];
         
@@ -30,15 +27,44 @@ function getCrystalValues() {
         }    
     }
 
-getCrystalValues(); 
+startGame();
+
+function resetGame() {
+ $(".myScore").html(0);
+ //$(".cyrstals").empty();
+ startGame();
+}
 
 //user clicks on a crystal
 $(".crystal").on("click", function(){
     var crystalNum = parseInt($(this).attr("data-randomNumber"));
     counter += crystalNum;
 
-    console.log(counter);
-});
+    if (counter === randomNumber) {
+       wins++;
+        $("#wins").html("Wins: " + wins);
+        replay = confirm("You won! Want to play again?");
+       
+        if (replay) {
+            resetGame();
+             }
+         }
+    else if (counter > randomNumber) {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        replay = confirm("Sorry, you lost. Want to play again?");
+        
+            if (replay) {
+            resetGame();
+            }
+ 
+        }
+
+    else if (counter < randomNumber) {
+        $(".myScore").html(counter);
+        
+    }
 
 });
 
+});
